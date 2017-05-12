7df1589c8a9d2e7c411f7ff2b60ebe6b9f417d0a
Michael-blondeau <color.ui>
2017-05-12 14:29:19 +0200

    Creation des classe cocktail et ustensile

---
+++ molotov/src/main/java/molotov/entities/Ustensile.java
@@ -0,0 +1,36 @@
+package molotov.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ustensiles")
+public class Ustensile {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable=false)
+	private String nom;
+
+	public Ustensile() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
